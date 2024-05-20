c99bca59cce8dde5c1515fc92a8c4182ec322a0b
Arkinefed <arkinefedmail@gmail.com>
2024-05-20 14:54:26 +0200

    finished about view

--- king-paimon-web/src/views/AboutView.vue
+++ king-paimon-web/src/views/AboutView.vue
@@ -1,14 +1,85 @@
 <template>
     <div class="about">
-        <h1>This is an about page</h1>
+        <p>
+            King Paimon to jedna z najbardziej znanych postaci wśród duchów opisywanych w
+            średniowiecznych grymuarach, takich jak "Ars Goetia" - pierwsza część "Lemegetonu",
+            znana również jako "Klucz Salomona". Postać Paimona fascynuje zarówno okultystów, jak i
+            entuzjastów demonologii, nie tylko ze względu na swoją moc, ale także na bogatą
+            symbolikę i różnorodne atrybuty.
+        </p>
+
+        <h1 class="red">Historia i Pochodzenie</h1>
+        <p>
+            Paimon pojawia się w różnych tekstach okultystycznych od wieków, a jego najbardziej
+            znane przedstawienie znajduje się w "Ars Goetia". Według tej księgi, Paimon jest jednym
+            z królów piekła, rządzącym 200 legionami duchów, zarówno dobrych, jak i złych. Jego
+            pozycja w hierarchii piekielnej czyni go jednym z najpotężniejszych i najbardziej
+            wpływowych demonów.
+        </p>
+
+        <h1 class="red">Atrybuty i Opis</h1>
+        <p>
+            Paimon jest zazwyczaj przedstawiany jako mężczyzna o kobiecej twarzy, noszący bogate
+            szaty i koronę, siedzący na wielbłądzie. Często towarzyszy mu procesja demonów grających
+            na instrumentach muzycznych, co podkreśla jego królewski status. W rękach trzyma berło
+            lub insygnia królewskie, co symbolizuje jego autorytet i władzę.
+        </p>
+
+        <h1 class="red">Moce i Zdolności</h1>
+        <p>
+            King Paimon jest znany ze swojej ogromnej wiedzy i mądrości. Okultyści wierzą, że
+            posiada głębokie zrozumienie nauk ścisłych, filozofii, sztuki, a także tajemnic natury.
+            Jest również mistrzem manipulacji i potrafi wpływać na wolę innych, co czyni go
+            szczególnie cennym sprzymierzeńcem dla tych, którzy potrafią go odpowiednio przywołać.
+        </p>
+
+        <h1>Przywoływanie i Współpraca</h1>
+        <p>
+            Przywołanie Paimona wymaga precyzyjnych rytuałów i odpowiedniego przygotowania. W
+            tradycji okultystycznej ważne jest, aby odbywało się to z szacunkiem i zgodnie z
+            odpowiednimi procedurami, aby uniknąć nieprzewidzianych konsekwencji. Paimon jest znany
+            z tego, że jest bardziej skłonny do współpracy z przywołującym, jeśli ten wykaże się
+            odpowiednią wiedzą i szacunkiem wobec jego potęgi.
+        </p>
+
+        <h1 class="red">Symbolika i Interpretacje</h1>
+        <p>
+            Paimon jest często interpretowany jako symbol wiedzy i mądrości, ale również jako postać
+            reprezentująca dualizm - łącząca w sobie elementy zarówno męskie, jak i żeńskie, dobro i
+            zło. Jego związek z muzyką i sztuką sugeruje, że jest patronem kreatywności i
+            inspiracji.
+        </p>
+
+        <h1 class="red">W Kulturze Popularnej</h1>
+        <p>
+            King Paimon zyskał również popularność w kulturze masowej. Jego postać pojawia się w
+            literaturze, filmach i grach wideo. Jednym z najbardziej znanych współczesnych odniesień
+            do Paimona jest film "Hereditary" (2018), w którym jest centralnym elementem fabuły.
+            Film ten przyczynił się do zwiększenia zainteresowania postacią Paimona i demonologią w
+            ogóle.
+        </p>
+
+        <h1 class="red">Podsumowanie</h1>
+        <p>
+            King Paimon pozostaje jedną z najbardziej fascynujących postaci w świecie okultyzmu.
+            Jego historia, moce i symbolika przyciągają zarówno uczonych, jak i laików, czyniąc go
+            niezwykle interesującą postacią do zgłębiania. Pomimo swojej demonicznej natury, Paimon
+            jest postacią złożoną i wielowymiarową, co czyni go wyjątkowym w panteonie duchów "Ars
+            Goetia".
+        </p>
     </div>
 </template>
 
-<style>
+<style scoped>
+p {
+    font-size: 1.1rem;
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+}
+
 @media (min-width: 1024px) {
     .about {
-        display: flex;
-        align-items: center;
+        text-align: justify;
         width: 1000px;
     }
 }
